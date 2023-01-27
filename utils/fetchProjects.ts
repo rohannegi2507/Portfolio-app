346c39e89206ade16313948282882ed00a714c44
rohan <rohan.negi.dev@gmail.com>
2023-01-28 03:48:35 +0530

    working project

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,7 +1,16 @@
 import { Project } from "../typing";
 
 export const fetchProjects = async()=>{
-  const res =  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)   
+  const header =  {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+      Accept: "application/json; charset=UTF-8",
+    },
+  }
+  const res =  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`, header)   
   const data = await res.json()
   const projects: Project[]= data.projects  
   return projects
